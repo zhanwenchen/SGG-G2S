338935021635934df285e5e45b62bd4a0cb4fbb3
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2022-05-16 18:15:49 +0000

    temporarily revert comm to original

--- maskrcnn_benchmark/utils/comm.py
+++ maskrcnn_benchmark/utils/comm.py
@@ -2,43 +2,28 @@
 This file contains primitives for multi-gpu communication.
 This is useful when doing distributed training.
 """
-from pickle import dumps as pickle_dumps, loads as pickle_loads
-from torch import (
-    cat as torch_cat,
-    no_grad as torch_no_grad,
-    stack as torch_stack,
-)
-from torch.cuda import (
-    LongTensor as torch_cuda_LongTensor,
-    ByteTensor as torch_cuda_ByteTensor,
-    ByteStorage as torch_cuda_ByteStorage,
-)
-from torch.distributed import (
-    is_available as dist_is_available,
-    is_initialized as dist_is_initialized,
-    get_world_size as dist_get_world_size,
-    get_rank as dist_get_rank,
-    barrier as dist_barrier,
-    reduce as dist_reduce,
-    all_gather as dist_all_gather,
-)
-torch_cuda_ByteStorage_from_buffer = torch_cuda_ByteStorage.from_buffer
+
+import pickle
+import time
+
+import torch
+import torch.distributed as dist
 
 
 def get_world_size():
-    if not dist_is_available():
+    if not dist.is_available():
         return 1
-    if not dist_is_initialized():
+    if not dist.is_initialized():
         return 1
-    return dist_get_world_size()
+    return dist.get_world_size()
 
 
 def get_rank():
-    if not dist_is_available():
+    if not dist.is_available():
         return 0
-    if not dist_is_initialized():
+    if not dist.is_initialized():
         return 0
-    return dist_get_rank()
+    return dist.get_rank()
 
 
 def is_main_process():
@@ -50,17 +35,17 @@ def synchronize():
     Helper function to synchronize (barrier) among all processes when
     using distributed training
     """
-    if not dist_is_available():
+    if not dist.is_available():
         return
-    if not dist_is_initialized():
+    if not dist.is_initialized():
         return
-    world_size = dist_get_world_size()
+    world_size = dist.get_world_size()
     if world_size == 1:
         return
-    dist_barrier()
+    dist.barrier()
 
 
-def all_gather(data):
+def all_gather(data): # TODO: optimize
     """
     Run all_gather on arbitrary picklable data (not necessarily tensors)
     Args:
@@ -76,14 +61,14 @@ def all_gather(data):
         return [data]
 
     # serialized to a Tensor
-    buffer = pickle_dumps(data)
-    storage = torch_cuda_ByteStorage_from_buffer(buffer)
-    tensor = torch_cuda_ByteTensor(storage).to(to_device)
+    buffer = pickle.dumps(data)
+    storage = torch.ByteStorage.from_buffer(buffer)
+    tensor = torch.ByteTensor(storage).to(to_device)
 
     # obtain Tensor size of each rank
-    local_size = torch_cuda_LongTensor([tensor.numel()]).to(to_device)
-    size_list = [torch_cuda_LongTensor([0]).to(to_device) for _ in range(world_size)]
-    dist_all_gather(size_list, local_size)
+    local_size = torch.LongTensor([tensor.numel()]).to(to_device)
+    size_list = [torch.LongTensor([0]).to(to_device) for _ in range(world_size)]
+    dist.all_gather(size_list, local_size)
     size_list = [int(size.item()) for size in size_list]
     max_size = max(size_list)
 
@@ -92,16 +77,16 @@ def all_gather(data):
     # gathering tensors of different shapes
     tensor_list = []
     for _ in size_list:
-        tensor_list.append(torch_cuda_ByteTensor(size=(max_size,)).to(to_device))
+        tensor_list.append(torch.ByteTensor(size=(max_size,)).to(to_device))
     if local_size != max_size:
-        padding = torch_cuda_ByteTensor(size=(max_size - local_size,)).to(to_device)
-        tensor = torch_cat((tensor, padding), dim=0)
-    dist_all_gather(tensor_list, tensor)
+        padding = torch.ByteTensor(size=(max_size - local_size,)).to(to_device)
+        tensor = torch.cat((tensor, padding), dim=0)
+    dist.all_gather(tensor_list, tensor)
 
     data_list = []
     for size, tensor in zip(size_list, tensor_list):
         buffer = tensor.cpu().numpy().tobytes()[:size]
-        data_list.append(pickle_loads(buffer))
+        data_list.append(pickle.loads(buffer))
 
     return data_list
 
@@ -118,18 +103,18 @@ def reduce_dict(input_dict, average=True):
     world_size = get_world_size()
     if world_size < 2:
         return input_dict
-    with torch_no_grad():
+    with torch.no_grad():
         names = []
         values = []
         # sort the keys so that they are consistent across processes
         for k in sorted(input_dict.keys()):
             names.append(k)
             values.append(input_dict[k])
-        values = torch_stack(values, dim=0)
-        dist_reduce(values, dst=0)
-        if dist_get_rank() == 0 and average:
+        values = torch.stack(values, dim=0)
+        dist.reduce(values, dst=0)
+        if dist.get_rank() == 0 and average:
             # only main process gets accumulated, so only divide by
             # world_size in this case
             values /= world_size
-        reduced_dict = dict(zip(names, values))
+        reduced_dict = {k: v for k, v in zip(names, values)}
     return reduced_dict
