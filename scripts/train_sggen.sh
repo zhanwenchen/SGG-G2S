2648c05a8f1ed2042c0885839c1f04787dd04651
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2022-09-07 12:28:48 -0400

    use .out instead of .log

--- scripts/train_sggen.sh
+++ scripts/train_sggen.sh
@@ -21,7 +21,7 @@ echo "TRAINING SGGen model ${MODEL_NAME}"
 cd ${PROJECT_DIR}
 MODEL_DIRNAME=${PROJECT_DIR}/checkpoints/${MODEL_NAME}/
 if [ -d "$MODEL_DIRNAME" ]; then
-  error_exit "Aborted: ${MODEL_DIRNAME} exists." 2>&1 | tee -a ${LOGDIR}/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.log
+  error_exit "Aborted: ${MODEL_DIRNAME} exists." 2>&1 | tee -a ${LOGDIR}/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.out
 fi
 mkdir ${MODEL_DIRNAME} &&
 cp -r ${PROJECT_DIR}/tools/ ${MODEL_DIRNAME} &&
