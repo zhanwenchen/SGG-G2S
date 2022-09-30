39ff1e3c4763217cd7e73c781549c10c69b72d57
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2022-09-30 16:56:22 -0400

    local version of slurm script for predcls

---
+++ scripts/train_predcls_local.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+timestamp() {
+  date +"%Y%m%d%T"
+}
+
+SLURM_JOB_NAME=pairwise_predcls_1GPU_legion_dev
+SLURM_JOB_ID=$(timestamp)
+
+error_exit()
+{
+#   ----------------------------------------------------------------
+#   Function for exit due to fatal program error
+#       Accepts 1 argument:
+#           string containing descriptive error message
+#   Source: http://linuxcommand.org/lc3_wss0140.php
+#   ----------------------------------------------------------------
+    echo "$(timestamp) ERROR ${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+    echo "$(timestamp) ERROR ${PROGNAME}: Exiting Early."
+    exit 1
+}
+
+error_check()
+{
+#   ----------------------------------------------------------------
+#   This function simply checks a passed return code and if it is
+#   non-zero it returns 1 (error) to the calling script.  This was
+#   really only created because I needed a method to store STDERR in
+#   other scripts and also check $? but also leave open the ability
+#   to add in other stuff, too.
+#
+#   Accepts 1 arguments:
+#       return code from prior command, usually $?
+#  ----------------------------------------------------------------
+    TO_CHECK=${1:-0}
+
+    if [ "$TO_CHECK" != '0' ]; then
+        return 1
+    fi
+
+}
+
+export PROJECT_DIR=/home/zhanwen/gsc
+export MODEL_NAME="${SLURM_JOB_ID}_${SLURM_JOB_NAME}"
+export LOGDIR=${PROJECT_DIR}/log
+MODEL_DIRNAME=${PROJECT_DIR}/checkpoints/${MODEL_NAME}/
+
+if [ -d "$MODEL_DIRNAME" ]; then
+  error_exit "Aborted: ${MODEL_DIRNAME} exists." 2>&1 | tee -a ${LOGDIR}/${SLURM_JOB_NAME}_${SLURM_JOB_ID}.log
+else
+  export CUDA_VISIBLE_DEVICES=0
+  export NUM_GPUS=1
+  export SEED=1234
+  export BATCH_SIZE=8
+  export MAX_ITER=15000
+  export LR=1e-3
+  export USE_GSC_FE=False
+  export CONFIG_FILE=configs/e2e_relation_X_101_32_8_FPN_1x_pairwise.yaml
+  export DATA_DIR_VG_RCNN=/home/zhanwen/datasets
+
+  ${PROJECT_DIR}/scripts/train_predcls.sh
+fi
